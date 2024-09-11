0406b5e4728240f57e0e137bef549cbf3f4f7caa
jlefevre559 <jlefevre559@gmail.com>
2024-09-11 15:28:47 -0600

    Fix urls (again)

--- Student/03/Superhero/hero/urls.py
+++ Student/03/Superhero/hero/urls.py
@@ -4,6 +4,6 @@ from .views import BlackWidow, HulkView, IndexView, IronManView
 urlpatterns = [
     path('', IndexView.as_view()),
     path('hulk', HulkView.as_view()),
-    path('ironman', IronManView.as_view()),
-    path('blackwidow', BlackWidow.as_view()),
+    path('iron_man', IronManView.as_view()),
+    path('black_widow', BlackWidow.as_view()),
 ]
