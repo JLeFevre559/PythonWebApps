eaa2a84e55ce33654059a7d76406b20fd9a4b1dd
jlefevre559 <jlefevre559@gmail.com>
2024-11-11 22:23:18 -0700

    Add str to investigator

--- Student/08/Superhero/hero/models.py
+++ Student/08/Superhero/hero/models.py
@@ -16,6 +16,9 @@ class Investigator(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
